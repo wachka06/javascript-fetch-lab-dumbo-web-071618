293362f1137b03784609116c8013b8fc17a3abd2
wachka06 <wachka06@gmail.com>
2018-09-04 09:32:52 -0400

    done

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ function createIssue() {
 }
 
 function showResults(json) {
+  document.querySelector('#results').innerHTML = json
 }
 
 function forkRepo() {
+  const url = 'https://developer.github.com/v3/repos/'
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  const token = getToken();
+
+  fetch(url + repo + '/forks', {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '38fac40dc67fe36b89b1a9ab867769407a0efec2';
 }
